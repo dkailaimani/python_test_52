1c6d4ead73b9005173d7ae148cc533d40417d64b
dkailaimani <102199241+dkailaimani@users.noreply.github.com>
2024-06-19 11:21:28 -0500

    Create home_component.jsx

---
+++ home_component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>Welcome to Our Store!</h1>
+            <Button variant="primary">Shop Now</Button>
+        </div>
+    );
+};
+
+export default HomePage;
